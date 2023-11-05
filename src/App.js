24fc74c95f11a0320241a456b503fb4883d9d0e1
kikos7 <chriseskaf@gmail.com>
2023-11-05 12:09:12 -0500

    Added child node and changed body background color

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import GreetUser from './components/GreetUser'
 
 const User1 = {
   name: 'Chris',
-  totalPoints: 24
+  totalPoints: 24,
+  questionsAnswered: 9
 }
 function App() {
   return (
     <div className="App">
-      <GreetUser name={User1.name} totalPoints={User1.totalPoints} />
+      <GreetUser name={User1.name} totalPoints={User1.totalPoints} questionsAnswered={User1.questionsAnswered} >
+        <p>this is a child prop</p>
+      </GreetUser>
     </div>
   );
 }
--- src/components/GreetUser.js
+++ src/components/GreetUser.js
@@ -3,7 +3,12 @@ import React from 'react'
 
 
 const Greet = (user) => {
-  return <h1>Hello {user.name}, you have {user.totalPoints} lifetime points.</h1>
+  return (
+    <div>
+      <h1>Hello {user.name}, you have {user.totalPoints} lifetime points.</h1>
+      <h3>{user.questionsAnswered} questions answered</h3>
+    </div>
+  )
 }
 
 export default Greet;
